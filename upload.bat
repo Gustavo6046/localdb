2098371d233719ef70c9cc5249195158ee0b5e99
Gustavo6046 <gugurehermann@gmail.com>
2017-10-10 19:29:06 -0300

    Fixed some bugs with append probably complete for 0.2.0

--- upload.bat
+++ upload.bat
@@ -6,7 +6,7 @@ shift
 :loop1
 if "%1"=="" goto after_loop
 if "%RESTVAR%"=="" (
-    set RESTVAR=%RESTVAR% %1
+    set RESTVAR=%1
 ) else (
     set RESTVAR=%RESTVAR% %1
 )
@@ -21,7 +21,7 @@ git add . -A
 git add . -u
 
 call npm run precompile
-echo ^> npm version %VTYPE% -m%RESTVAR% --force
-call npm version %VTYPE% -m%RESTVAR% --force
+echo ^> npm version %VTYPE% -m %RESTVAR% --force
+call npm version %VTYPE% -m %RESTVAR% --force
 call npm run oncompile
 call npm publish
